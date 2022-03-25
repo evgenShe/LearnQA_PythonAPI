c4efbb2f88ab701dd845f66a71e4e8c369f9bf01
evgeniysheremet <e.sheremet@ibolit.pro>
2022-03-25 21:10:32 +0300

    fix

--- Ex5_pars_json.py
+++ Ex5_pars_json.py
@@ -2,4 +2,7 @@ import json
 
 json_text = '{"messages": [{"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"}, {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}'
 obj = json.loads(json_text)
-print(obj['messages'][1])
+
+key1 = "messages"
+key2 = "message"
+print(obj[key1][1][key2])
