7c57eb0f568419196dfa02892b3a9d113c9be891
evgeniysheremet <e.sheremet@ibolit.pro>
2022-03-25 20:49:15 +0300

    fix

--- Ex5_pars_json.py
+++ Ex5_pars_json.py
@@ -1,5 +1,5 @@
 import json
 
-some_json = '{"messages": [{"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"}, {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}'
-obj = json.loads(some_json)
+json_text = '{"messages": [{"message": "This is the first message", "timestamp": "2021-06-04 16:40:53"}, {"message": "And this is a second message", "timestamp": "2021-06-04 16:41:01"}]}'
+obj = json.loads(json_text)
 print(obj['messages'][1])
