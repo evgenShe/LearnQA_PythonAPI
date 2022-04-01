16ef8103c44a9505a27c9bfdeb3ced239c9a18be
evgeniysheremet <e.sheremet@ibolit.pro>
2022-04-01 18:34:57 +0300

    update

--- Ex8_tokens.py
+++ Ex8_tokens.py
@@ -1,6 +1,15 @@
+import time
+
 import requests
 import json
 
 response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
 response_asjson = response.json()
 print(response_asjson)
+token = response_asjson['token']
+response_with_token = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": f"{token}"})
+print(response_with_token.text)
+time_wait = response_asjson['seconds']
+time.sleep(time_wait)
+response_with_token = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": f"{token}"})
+print(response_with_token.text)
