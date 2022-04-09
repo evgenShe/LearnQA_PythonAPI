59e0783556ad241c2d2a2f1e97204aa4cd93cc91
evgeniysheremet <e.sheremet@ibolit.pro>
2022-04-09 20:03:07 +0300

    ex8

--- Ex8_tokens.py
+++ Ex8_tokens.py
@@ -1,15 +1,28 @@
 import time
 
 import requests
-import json
 
 response = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
 response_asjson = response.json()
-print(response_asjson)
+# print(response_asjson)
+# {'token': 'wNxoTMwoDMyASOw0CNw0iMyAjM', 'seconds': 4}
+
 token = response_asjson['token']
 response_with_token = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": f"{token}"})
-print(response_with_token.text)
+# print(response_with_token.text)
+# {"status":"Job is NOT ready"}
+
+response_with_token_as_json = response_with_token.json()
+checkStatus = response_with_token_as_json['status']
+if checkStatus != "Job is NOT ready":
+    print("Status is NOT is true")
+
 time_wait = response_asjson['seconds']
 time.sleep(time_wait)
-response_with_token = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={"token": f"{token}"})
-print(response_with_token.text)
+response_with_token2 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job",
+                                    params={"token": f"{token}"})
+check_status_and_result = response_with_token2.json()
+if check_status_and_result['status'] == "Job is ready" and check_status_and_result['result'] == check_status_and_result[
+    'result']:
+    print(check_status_and_result)
+    # {'result': '42', 'status': 'Job is ready'}
